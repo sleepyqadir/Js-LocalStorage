36c9042f8157414adedc5a65d07433389f3a84dc
Abdul Qadir <a.qadir67@outlook.com>
2019-06-21 15:59:24 +0500

    finalized the functionality of notes-app and connected local storage

--- notes-app/notesApp.js
+++ notes-app/notesApp.js
@@ -1,29 +1,67 @@
-let log = console.log;
+
+////////////////////////////////////////////
+//
+// Dom access
+//
+/////////////////////////////////////////////
+
 const form = document.querySelector("form");
 const clear = document.querySelector("button");
 const items = document.querySelector("ul");
 const input = document.querySelector("input");
+
+////////////////////////////////////////////
+//
+// values
+//
+/////////////////////////////////////////////
+
+let log = console.log;
+let data = []
+
+/////////////////////////////////////////////
+//
+// local storage
+//
+/////////////////////////////////////////////
+
+let localData = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [] 
+
+////////////////////////////////////////////
+//
+// functions
+//
+/////////////////////////////////////////////
 
 const itemMaker = text => {
   const item = document.createElement("li");
   item.innerHTML = text;
   items.appendChild(item);
 };
-itemMaker("hellooo")
-itemMaker("hellooo")
-itemMaker("hellooo")
-itemMaker("hellooo")
-itemMaker("hellooo")
 
 const clearAll = () => {
     items.innerHTML = ""
+    localStorage.clear()
 }
+
+
+localData.forEach((item)=>{
+    itemMaker(item)
+})
+
+////////////////////////////////////////////
+//
+// event listeners
+//
+/////////////////////////////////////////////
 
 clear.addEventListener("click",clearAll)
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
-
+    data.push(input.value)
+    localStorage.setItem("items",JSON.stringify(data))
     itemMaker(input.value)
     input.value = ""
 })
+
